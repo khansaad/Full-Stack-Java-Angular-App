abcb171db089b788f721cea4a7cd2103aecfc9af
Saad Khan <khan.ssaad@gmail.com>
2021-07-12 18:29:29 +0530

    Added New Todo button, updated 'Update' button integration with UI'

--- frontend/todo/src/app/todo/todo.component.ts
+++ frontend/todo/src/app/todo/todo.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Todo } from '../list-todos/list-todos.component';
 import { TodoDataService } from '../service/data/todo-data.service';
 
@@ -14,17 +14,49 @@ export class TodoComponent implements OnInit {
   todo: Todo
   constructor(
     private todoService: TodoDataService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private router: Router
   ) { }
 
   ngOnInit() {
-    this.id = this.route.snapshot.params['id'];
-    this.todo = new Todo(1, "Learn something", false, new Date())
-    this.todoService.retrieveTodo('merc', this.id)
-    .subscribe(
-      data => this.todo = data
 
-    )
+    this.id = this.route.snapshot.params['id'];
+    
+    this.todo = new Todo(this.id, '', false, new Date())
+
+    if(this.id != -1){
+      this.todoService.retrieveTodo('merc', this.id)
+      .subscribe(
+        data => this.todo = data
+
+      )
+    }
   }
+
+  
+  saveTodo(){
+    if(this.id === -1){
+      //create todo
+      this.todoService.createTodo('merc', this.todo)
+      .subscribe (
+        data => {
+          console.log(data)
+          this.router.navigate(['todos'])
+        }
+      )
+
+    } else{
+      
+      this.todoService.updateTodo('merc', this.id, this.todo)
+        .subscribe (
+          data => {
+            console.log(data)
+            this.router.navigate(['todos'])
+          }
+        )
+      
+    }
+  }
+
 
 }
--- restful-web-services/src/main/java/com/mercsoftwares/rest/webservices/restfulwebservices/TodoResource.java
+++ restful-web-services/src/main/java/com/mercsoftwares/rest/webservices/restfulwebservices/TodoResource.java
@@ -1,14 +1,20 @@
 package com.mercsoftwares.rest.webservices.restfulwebservices;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @CrossOrigin(origins="http://localhost:4200/")
 @RestController
@@ -21,6 +27,28 @@ public class TodoResource {
 	public List<Todo> getAllTodos(@PathVariable String username){
 		
 		return todoService.findAll();
+	}
+
+	@PutMapping("/users/{username}/todos/{id}")
+	public ResponseEntity<Todo> updateTodo(
+			@PathVariable String username,
+			@PathVariable long id,@RequestBody Todo todo){
+		
+		Todo todoUpdated = todoService.save(todo);
+		
+		return new ResponseEntity<Todo>(todoUpdated, HttpStatus.OK);
+	}
+	
+	@PostMapping("/users/{username}/todos")
+	public ResponseEntity<Todo> createTodo(
+			@PathVariable String username, @RequestBody Todo todo){
+		
+		Todo createdTodo = todoService.save(todo);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+		.buildAndExpand(createdTodo.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();
 	}
 	
 	@GetMapping("/users/{username}/todos/{id}")
