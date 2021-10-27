75f41824749627cb2b60d4dd08b507cc24b42e64
Saad Khan <khan.ssaad@gmail.com>
2021-10-27 18:16:47 +0530

    Updated Id Datatype, app.properties

--- restful-web-services/src/main/java/com/mercsoftwares/rest/webservices/restfulwebservices/BcryptEncoderTest.java
+++ restful-web-services/src/main/java/com/mercsoftwares/rest/webservices/restfulwebservices/BcryptEncoderTest.java
@@ -6,8 +6,12 @@ public class BcryptEncoderTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		String randomPass = "pass@321#";
 		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-		System.out.println(encoder.encode("pass@321#"));
+		String endodedPass = encoder.encode(randomPass);
+		
+		System.out.println("encoded: "+endodedPass);
+		System.out.println("decoder: "+ encoder.matches( randomPass, endodedPass));
 		
 	}
 
